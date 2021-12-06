fa74d26b8ed958976923ec962c700fd11f90c127
0724654276 <you@example.com>
2021-12-06 09:57:45 +0300

    configure app database settings to psql

--- instagram/settings.py
+++ instagram/settings.py
@@ -83,9 +83,11 @@ WSGI_APPLICATION = 'instagram.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gram',
+        'USER': 'geroge',
+    'PASSWORD':'kamakia91',
     }
 }
 
